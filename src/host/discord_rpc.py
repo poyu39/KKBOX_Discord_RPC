ef232091fd181f12a6514986936529b98c6036f3
Poyu <42506064+poyu39@users.noreply.github.com>
2023-09-09 23:34:55 +0800

    feat: 更改 small_image 和 small_text

--- src/host/discord_rpc.py
+++ src/host/discord_rpc.py
@@ -18,8 +18,8 @@ class DiscordRPC:
             'details': f"{kkbox_data['song_name']} ",
             'large_text': f'正在聽: {kkbox_data["song_name"]}',
             'large_image': kkbox_data['song_image'],
-            'small_image': 'https://i.imgur.com/tUEa4Xz.png',
-            'small_text': 'KKBOX',
+            'small_image': 'https://i.imgur.com/BOIijD9.png',
+            'small_text': 'KKBOX Discord RPC',
             'buttons': [
                 {
                     'label': f'{kkbox_data["song_time"][0]} / {kkbox_data["song_time"][1]}',
